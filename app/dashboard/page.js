1546197d20a1a4bc22e6c459eb6bd89bbacd433e
AmitxParmar <amitparmar901@gmail.com>
2023-06-28 01:25:31 +0530

    integrate api

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,23 +1,45 @@
 "use client";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { UserAuth } from "../hooks/useAuth";
 import { useRouter } from "next/navigation";
 
 const Dashboard = () => {
-
+  const [todos, setTodos] = useState([]);
   const router = useRouter();
   const { user } = UserAuth();
 
-  React.useEffect(() => {
+  useEffect(() => {
     !user ? router.push("/") : null;
+    fetch("https://jsonplaceholder.typicode.com/todos/1")
+      .then((response) => response.json())
+      .then((json) => setTodos(json));
   }, []);
 
+  const handleSave = async (id) => {
+    const task = todos.find((todo) => item.id === id);
+
+    try {
+      const docRef = await addDoc(collectionRef, task);
+      console.log("Object saved with ID:", docRef.id);
+    } catch (error) {
+      console.error("Error saving object:", error);
+    }
+  };
+
   return (
-    <div className="min-h-screen py-12 px-6">Hello, {user?.displayName}
-    
-      <div className="bg-gray-500/30">
-        
-    </div>
+    <div className="min-h-screen py-12 px-6">
+      Hello, {user?.displayName}
+      <div className="bg-gray-500/30 rounded-lg">
+        {todos?.map((todo) => (
+          <div
+            className="bg-gray-100"
+            onClick={() => handleSave(todo.id)}
+            key={todo.id}
+          >
+            <h3>{todo.title}</h3>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
